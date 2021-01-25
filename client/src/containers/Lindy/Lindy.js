718ef2dfbfe34f305133aee9c68d06f8dfc26c2b
Sjaak <sjaakrest@gmail.com>
2021-01-25 11:22:14 +0100

    replace isMobile with package and fix screen height on /lindy

--- client/src/containers/Lindy/Lindy.js
+++ client/src/containers/Lindy/Lindy.js
@@ -1,29 +1,14 @@
 import React, { useState } from "react";
+import { isMobile } from "react-device-detect";
 
 import "./Lindy.scss";
 
 const Lindy = () => {
     const [hearts, setHearts] = useState([]);
 
-    const isMobile = () => {
-        const toMatch = [
-            /Android/i,
-            /webOS/i,
-            /iPhone/i,
-            /iPad/i,
-            /iPod/i,
-            /BlackBerry/i,
-            /Windows Phone/i,
-        ];
-
-        return toMatch.some((toMatchItem) => {
-            return navigator.userAgent.match(toMatchItem);
-        });
-    };
-
-    const onEvent = (e) => {
+    const addHeart = (e) => {
         const newHeart = {
-            top: `${e.clientY - 60}px`,
+            top: `${e.clientY - 120}px`,
             left: `${e.clientX - 60}px`,
         };
 
@@ -46,11 +31,16 @@ const Lindy = () => {
         <div
             className="lindy"
             id="lindy"
-            onClick={isMobile() ? onEvent : () => {}}
-            onMouseMove={isMobile() ? () => {} : onEvent}
+            onClick={
+                isMobile
+                    ? addHeart
+                    : () => {
+                          setHearts([]);
+                      }
+            }
+            onMouseMove={isMobile ? () => {} : addHeart}
         >
             {children}
-            <div></div>
         </div>
     );
 };
