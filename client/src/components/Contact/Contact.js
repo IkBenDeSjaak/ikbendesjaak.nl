bc08a6838fca481c38851254843be9ddb9dee898
Sjaak <sjaakrest@gmail.com>
2021-01-31 17:32:49 +0100

    update contact links

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import IconButton from "@material-ui/core/IconButton";
 import MailIcon from "@material-ui/icons/Mail";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import Title from "../shared/Title/Title";
@@ -13,7 +14,16 @@ function Contact() {
             <div className={styles.links}>
                 <div className={styles.link}>
                     <a href="mailto:sjaakrest@gmail.com">
+                        {/* <IconButton
+                        className={styles.button}
+                        onClick={() =>
+                            (window.location.href =
+                                "mailto:sjaakrest@gmail.com")
+                        }
+                    > */}
+                        <span>Send me an e-mail</span>
                         <MailIcon />
+                        {/* </IconButton> */}
                     </a>
                 </div>
                 <div className={styles.link}>
@@ -22,7 +32,17 @@ function Contact() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
+                        {/* <IconButton
+                        className={styles.button}
+                        onClick={() =>
+                            window.open(
+                                "https://www.linkedin.com/in/sjaak-kok/"
+                            )
+                        }
+                    > */}
+                        <span>Visit my LinkedIn</span>
                         <LinkedInIcon />
+                        {/* </IconButton> */}
                     </a>
                 </div>
             </div>
