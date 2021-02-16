49ee0629192021feb06214a96967d7e0cb404b4c
Sjaak <sjaakrest@gmail.com>
2021-02-16 21:08:24 +0100

    add svg file for all skills

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./Skills.module.scss";
 
 import Title from "../shared/Title/Title";
+import { blue } from "@material-ui/core/colors";
 
 function Skills() {
     return (
@@ -11,45 +12,30 @@ function Skills() {
             <p>These are some technologies I've been working with:</p>
             <div className={styles.content}>
                 <div>
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-                        alt="React logo"
-                    ></img>
+                    <img src="/skills-images/react.svg" alt="React logo"></img>
                     <p>React</p>
                 </div>
                 <div>
-                    <img
-                        src="https://img.icons8.com/color/452/nodejs.png"
-                        alt="Node.js logo"
-                    ></img>
+                    <img src="/skills-images/node.svg" alt="Node.js logo"></img>
                     <p>Node.js</p>
                 </div>
                 <div>
                     <img
-                        src="https://hackr.io/tutorials/learn-express-js/logo/logo-express-js?ver=1557508379"
-                        alt="RExpress.js logo"
+                        src="/skills-images/express.svg"
+                        alt="Express.js logo"
                     ></img>
                     <p>Express.js</p>
                 </div>
                 <div>
-                    <img
-                        src="https://www.mica.nl/wp-content/uploads/2020/02/CSS-3-Logo-1.png"
-                        alt="CSS logo"
-                    ></img>
+                    <img src="/skills-images/css.svg" alt="CSS logo"></img>
                     <p>CSS</p>
                 </div>
                 <div>
-                    <img
-                        src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg"
-                        alt="SQL logo"
-                    ></img>
+                    <img src="/skills-images/mssql.svg" alt="SQL logo"></img>
                     <p>SQL</p>
                 </div>
                 <div>
-                    <img
-                        src="https://cdn.worldvectorlogo.com/logos/php.svg"
-                        alt="PHP logo"
-                    ></img>
+                    <img src="/skills-images/php.svg" alt="PHP logo"></img>
                     <p>PHP</p>
                 </div>
             </div>
