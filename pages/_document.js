3b8384f3ce3b6e5050194cf7ab0053fc8d63b291
Sjaak <sjaakrest@gmail.com>
2022-06-30 18:30:14 +0200

    fix hamburger menu icon not showing

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ export default function Document() {
         <meta name='twitter:creator' content='@IkBenDeSjaak99' />
         <link rel='apple-touch-icon' href='/favicon.png' />
         <link
-          rel='preconnect'
+          rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css'
         />
         <link rel='manifest' href='/manifest.json' />
